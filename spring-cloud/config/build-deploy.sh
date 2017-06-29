2ae7f75bbc87173096c3831fcd47b0c362253773
kbastani <kb@socialmoon.com>
2017-06-29 07:04:23 -0400

    Update README

--- spring-cloud/config/build-deploy.sh
+++ spring-cloud/config/build-deploy.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-rm -r ./target
+rm -rf ./target
 mkdir target
 
 spring jar --classpath application.yml target/configserver.jar configserver.groovy
 
 cf push
 
-cf cups config-server -p '{"uri":"http://todo-config-server.cfapps.io"}'
+SERVICE_URI=$(cf curl /v2/apps/$(cf app config-server --guid)/env | jq -r '.application_env_json.VCAP_APPLICATION.uris[0]')
+
+cf cups config-server -p "{\"uri\":\"http://$SERVICE_URI\"}"
--- spring-cloud/discovery/build-deploy.sh
+++ spring-cloud/discovery/build-deploy.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-rm -r ./target
+rm -rf ./target
 mkdir target
 
 spring jar --classpath application.yml target/eureka.jar eureka.groovy
 
 cf push
 
-cf cups discovery-service -p '{"uri":"http://todo-discovery-service.cfapps.io"}'
+SERVICE_URI=$(cf curl /v2/apps/$(cf app discovery-service --guid)/env | jq -r '.application_env_json.VCAP_APPLICATION.uris[0]')
+
+cf cups discovery-service -p "{\"uri\":\"http://$SERVICE_URI\"}"
